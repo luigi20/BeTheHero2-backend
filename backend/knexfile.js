260200076b4994f360c097d017be6bc7418799d0
Luis <luisopentec@gmail.com>
2021-11-28 19:24:04 -0300

    change variables

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -10,7 +10,10 @@ module.exports = {
       user: process.env.USER_NAME,
       password: process.env.PASSWORD,
       url: process.env.DATABASE_URL,
-      host: process.env.HOST
+      host: process.env.HOST,
+      ssl: {
+        rejectUnauthorized: false
+      }
     },
     pool: {
       min: 2,
