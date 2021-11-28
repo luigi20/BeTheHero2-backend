d71c243875e62ffa8fb097f72fecce0c1fb76e87
Luis <luisopentec@gmail.com>
2021-11-28 20:25:20 -0300

    cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const routes = require('./routes');
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: 'https://bethehero2021-frontend.herokuapp.com' }));
 /*
     Tipos de parametros
     Query Param : Parametros nomeados enviados na rota após "?"
@@ -14,7 +14,4 @@ app.use(cors());
 
 app.use(express.json());
 app.use(routes);
-
-
-
 app.listen(process.env.PORT || 3333);
